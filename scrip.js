23e2590c905e0a8679f8853f50af8e3c71abd296
Eli-o7 <araujoeliana28@gmail.com>
2025-09-27 14:38:03 +0000

    4

--- scrip.js
+++ scrip.js
@@ -1,9 +1,6 @@
-// Selecciona el elemento con el id "menu-icon" y agrega un evento de clic
-document.getElementById("menu-icon").addEventListener("click", function () {
-  // Dentro del evento de clic, selecciona el elemento con el id "menu"
-  const menu = document.getElementById("menu");
+const toggleButton = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
-  // Alterna la clase "hidden" en el elemento "menu"
-  // Si la clase "hidden" está presente, la elimina; si no está, la agrega
-  menu.classList.toggle("hidden");
-});
+    toggleButton.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
